38dd016001fb487d49f5b0be47706a686bd58be6
AyushSinghAryan”git config --global user.email “ayush.singh2019@vitbhopal.ac.in <ayush.singh2019@vitbhopal.ac.in>
2026-01-08 18:43:56 +0530

    day-3 work

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import Skills from "./components/Skills"
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <div>
         <Hero />
         <Navbar />
+        <Skills />
       </div>
     </>
   )
